264b2ab4137cf0a139a2522ee7872a86e1a5db71
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-10-01 15:42:38 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    X6 += -1
+    qwiicgpio.writeOUTPUT_PORT(i2cAdr, X6)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    X6 += 1
+    qwiicgpio.writeOUTPUT_PORT(i2cAdr, X6)
+})
+input.onButtonEvent(Button.A, ButtonEvent.Hold, function () {
+    basic.showNumber(qwiicgpio.bitwise(qwiicgpio.readINPUT_PORT(i2cAdr), qwiicgpio.eBit.AND, 7))
+})
+let X6 = 0
+let i2cAdr = 0
+i2cAdr = qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x26)
 qwiicgpio.setMode(
-qwiicgpio.eADDR.GPIO_Qwiic,
+i2cAdr,
 qwiicgpio.eIO.IN,
 qwiicgpio.eIO.IN,
 qwiicgpio.eIO.IN,
 qwiicgpio.eIO.IN,
 qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT
 )
